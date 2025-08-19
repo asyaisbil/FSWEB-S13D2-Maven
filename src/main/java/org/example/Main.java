205126cd317cf100bec127d6caf5d62869e6ab06
asyaisbil <asyaisbill@gmail.com>
2025-08-19 18:06:04 +0300

    First Q

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
+    public static boolean isPalindrome(int integer){
+        if(integer >= 0){
+            String integerStr = Integer.toString(integer);
+            for(int i = 0; i < integerStr.length(); i++){
+                if(integerStr[i] == integerStr[integerStr.length()-1]){
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
 }
+
+
